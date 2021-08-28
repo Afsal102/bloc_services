5f8e4d7799cebcc9eb5c33017c8281a0e6d36bd0
Afsal102 <39826977+Afsal102@users.noreply.github.com>
2021-08-28 11:25:10 +0530

    removed comments

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -14,7 +14,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: ExampleView(),
-      // home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
